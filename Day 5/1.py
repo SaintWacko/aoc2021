83d1e33ac683a8e1524b2c2d7c83ed652ee5d397
Alex Tirrell <saintwacko@gmail.com>
2021-12-05 10:03:26 -0600

    Day 5

---
+++ Day 5/1.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def main():
+    with open('input', 'r') as file:
+        lines = file.read().splitlines()
+        coords = [np.array([[int(n) for n in l.split(',')] for l in line.split(' -> ')]) for line in lines]
+        max = np.amax(coords)
+        grid = np.zeros(shape=(max + 1, max + 1))
+        for coord in coords:
+            if any(np.all(coord == coord[0, :], axis=0)):
+                rot = list(zip(*coord))
+                cols = sorted(rot[0])
+                rows = sorted(rot[1])
+                grid[rows[0]:rows[1] + 1, cols[0]:cols[1] + 1] = grid[rows[0]:rows[1] + 1, cols[0]:cols[1] + 1] + 1
+        return((grid > 1).sum())
+
+
+if __name__ == "__main__":
+    print(main())
---
+++ Day 5/2.py
@@ -0,0 +1,28 @@
+import numpy as np
+from pandas import DataFrame
+
+
+def main():
+    with open('input', 'r') as file:
+        lines = file.read().splitlines()
+        coords = [np.array([[int(n) for n in l.split(',')] for l in line.split(' -> ')]) for line in lines]
+        max = np.amax(coords)
+        grid = np.zeros(shape=(max + 1, max + 1))
+        for coord in coords:
+            rot = list(zip(*coord))
+            cols = sorted(rot[0])
+            rows = sorted(rot[1])
+            if any(np.all(coord == coord[0, :], axis=0)):
+                grid[rows[0]:rows[1] + 1, cols[0]:cols[1] + 1] = grid[rows[0]:rows[1] + 1, cols[0]:cols[1] + 1] + 1
+            else:
+                if (rot[0][0] > rot[0][1]) == (rot[1][0] > rot[1][1]):
+                    np.fill_diagonal(grid[rows[0]:rows[1] + 1, cols[0]:cols[1] + 1], grid[rows[0]:rows[1] + 1, cols[0]:cols[1] + 1].diagonal() + 1)
+                else:
+                    np.fill_diagonal(np.fliplr(grid[rows[0]:rows[1] + 1, cols[0]:cols[1] + 1]), np.fliplr(grid[rows[0]:rows[1] + 1, cols[0]:cols[1] + 1]).diagonal() + 1)
+        return((grid > 1).sum())
+
+
+if __name__ == "__main__":
+    print(main())
+
+
